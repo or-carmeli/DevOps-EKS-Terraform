1b70ed7d5b91f04b0603cfecd90e2a9cdca2631b
root <ocarmeli7@gmail.com>
2023-11-20 10:49:24 +0200

    Add terraform lock file for provider version locking

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,6 +45,8 @@ resource "aws_instance" "kubernetes_instance" {
   instance_type = var.instance_type
   subnet_id     = aws_subnet.private_subnet.id
 
+  vpc_security_group_ids = [aws_security_group.private_sg.id]
+
   tags = {
     Name = "KubernetesInstance"
   }
@@ -53,7 +55,8 @@ resource "aws_instance" "kubernetes_instance" {
 resource "aws_instance" "bastion_host" {
   ami           = var.instance_ami
   instance_type = var.instance_type
-  subnet_id     = aws_subnet.public.id
+  subnet_id     = aws_subnet.public_subnet.id
+  key_name      = "KeysForMachines"
 
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
 
@@ -63,27 +66,15 @@ resource "aws_instance" "bastion_host" {
 }
 
 resource "aws_security_group" "private_sg" {
-  vpc_id = aws_vpc.main_vpc.id
-
-  // SSH access
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [aws_instance.bastion_host.private_ip]
-  }
-}
-
-resource "aws_security_group" "bastion_sg" {
-  name        = "bastion-sg"
-  description = "Security group for Bastion Host"
-  vpc_id      = aws_vpc.main.id
+  name        = "private-sg"
+  description = "Security group for Private Instances"
+  vpc_id      = aws_vpc.main_vpc.id
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["your_ip_address/32"]  # Replace with your IP address
+    security_groups = [aws_security_group.bastion_sg.id]
   }
 
   egress {
@@ -94,3 +85,95 @@ resource "aws_security_group" "bastion_sg" {
   }
 }
 
+resource "aws_security_group" "bastion_sg" {
+  name        = "bastion-sg"
+  description = "Security group for Bastion Host"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Replace with your IP address
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "nginx_alb" {
+  name               = "nginx-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public_subnet.id]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "nginx-alb"
+  }
+}
+
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Security group for ALB"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "nginx_target_group" {
+  name     = "nginx-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main_vpc.id
+
+  health_check {
+    enabled             = true
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200-299"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "NginxTargetGroup"
+  }
+}
+
+resource "aws_lb_listener" "nginx_listener" {
+  load_balancer_arn = aws_lb.nginx_alb.arn 
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nginx_target_group.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "nginx_attachment" {
+  target_group_arn = aws_lb_target_group.nginx_target_group.arn
+  target_id        = aws_instance.kubernetes_instance.id 
+  port             = 80
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,13 +13,32 @@ output "private_subnet_id" {
   value       = aws_subnet.private_subnet.id
 }
 
-output "ec2_instance_id" {
-  description = "The ID of the EC2 instance"
+output "kubernetes_instance_id" {
+  description = "The ID of the Kubernetes EC2 instance"
   value       = aws_instance.kubernetes_instance.id
 }
 
-output "security_group_id" {
-  description = "The ID of the security group"
-  value       = aws_security_group.main_sg.id
+output "bastion_host_instance_id" {
+  description = "The ID of the Bastion Host instance"
+  value       = aws_instance.bastion_host.id
 }
 
+output "bastion_host_public_ip" {
+  description = "The public IP of the Bastion Host"
+  value       = aws_instance.bastion_host.public_ip
+}
+
+output "nginx_alb_arn" {
+  description = "The ARN of the NGINX Application Load Balancer"
+  value       = aws_lb.nginx_alb.arn 
+}
+
+output "nginx_target_group_arn" {
+  description = "The ARN of the NGINX Target Group"
+  value       = aws_lb_target_group.nginx_target_group.arn
+}
+
+output "nginx_listener_arn" {
+  description = "The ARN of the NGINX Listener"
+  value       = aws_lb_listener.nginx_listener.arn
+}
