4cff24baa5a82167149699cad7aa9d1cb7cea963
root <ocarmeli7@gmail.com>
2023-11-22 13:06:08 +0200

    Changes in Terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -76,6 +76,7 @@ resource "aws_subnet" "public_subnet_2" {
 resource "aws_subnet" "private_subnet" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = var.private_subnet_cidr
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = false
 
   tags = {
